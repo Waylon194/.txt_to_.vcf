2d6f9019042347c2f778f4205258cc3041de49f8
Waylon <waylon194@hotmail.com>
2022-07-01 20:10:07 +0200

    V1.0
    Added source code and example

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # .txt_to_.vcf
+Place your data into input.txt (inside the Example folder) like this:
+Example One,+31 6 12345678
+Example Two,+31 6 12345678
+
+Run Text_to_Vcf_generator.exe and your output(.vcf) will be generated.
+
+I used the following template:
+BEGIN:VCARD
+VERSION:4.0
+FN:
+TEL; WORK; VOICE:
+END:VCARD
+
+Which is a bare minimum, but you can edit it to your needs (files must be placed inside .txt_to_.vcf\Text_to_Vcf_generator\bin\Debug).
---
+++ Text_to_Vcf_generator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Text_to_Vcf_generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputData = File.ReadLines("input.txt");
+            using (StreamWriter writer = new StreamWriter("output.vcf")) {
+                foreach (var person in inputData)
+                {
+                    int length = person.IndexOf(",");
+                    var name = person.Substring(0, length);
+                    var number = person.Substring(length + 1, 14);
+
+                    Console.WriteLine("BEGIN:VCARD\nVERSION:4.0\nFN:" + name + "\nTEL; WORK; VOICE:" + number + "\nEND:VCARD");
+                    writer.WriteLine("BEGIN:VCARD\nVERSION:4.0\nFN:" + name + "\nTEL; WORK; VOICE:" + number + "\nEND:VCARD");
+                }
+            }
+            Console.WriteLine("\n\tAll done! see 'output.vcf'\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
